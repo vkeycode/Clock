ad55fe540a36c8aeb098205316ed02c199caf0ae
Vedat Eroglu <vdterog@gmail.com>
2022-10-10 23:48:15 +0300

    fix

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const hourHand = document.querySelector(".hour")
 function setDate() {
 
     const now = new Date(); // Get Date
+    
     const seconds = now.getSeconds(); // Get Seconds
     const secondsDegree = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegree}deg)` 
@@ -16,7 +17,7 @@ function setDate() {
     const hours = now.getHours(); // Get Hours
     const hoursDegree = ((hours / 12) * 360) + 90 
     hourHand.style.transform = `rotate(${hoursDegree}deg)`
-    console.log(hours)
+
 }
 
 setInterval(setDate, 1000)
